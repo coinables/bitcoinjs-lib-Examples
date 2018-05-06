b7acb95754c31fa3f0d8d2b1e4368414c29b9117
Kyle Honeycutt <coinables@gmail.com>
2018-05-06 11:28:38 -0700

    p2wpkh & p2pkh simple examples

---
+++ p2pkh.js
@@ -0,0 +1,30 @@
+let bitcoin = require("bitcoinjs-lib");
+
+//creating and signing a standard p2pkh transaction
+
+let NETWORK = bitcoin.networks.bitcoin; 
+var txb = new bitcoin.TransactionBuilder(NETWORK);
+
+//get unspent output details
+let txid = ""; //transaction id
+let outn = 0;  // n out
+let outvalue = 40000; // output value in satoshis
+
+//add input
+txb.addInput(txid, outn);
+
+//add output
+txb.addOutput("bc1qmwzvnazs8263g6v9ctscwheu329dvsax6jl369",35000); //first argument is address that will receive the funds, the second is the value to send in satoshis after deducting the mining fees. In this example there are 5000 satoshis in mining fees (40000-35000=5000)
+
+//signing
+let WIF = ""; //private key of the address associated with this unspent output
+let keypair = bitcoin.ECPair.fromWIF(WIF, NETWORK);
+txb.sign(0, keypair);
+let tx = txb.build();
+let txhex = tx.toHex();
+
+console.log (txhex);
+
+//using buildIncomplete lets you build your transaction before its signed or complete, good for debugging or multi signings
+//let txhex = txb.buildIncomplete().toHex();
+//console.log(txhex);
